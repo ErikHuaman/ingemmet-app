36bdbd3ce9f4cfd3a36cf5c46fabc95702a07cec
YonerCruz <yonelcruz07@gmail.com>
2022-09-15 13:56:19 -0500

    Comp. Redes Sociales listo

--- src/app/modules/pages/sistema-de-gestion/view/components/redes-sociales/redes-sociales.component.ts
+++ src/app/modules/pages/sistema-de-gestion/view/components/redes-sociales/redes-sociales.component.ts
@@ -5,6 +5,29 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './redes-sociales.component.html',
 })
 export class RedesSocialesComponent implements OnInit {
+  redeSociales = [
+    {
+      red: 'facebook',
+      url: 'https://img.icons8.com/color/150/000000/facebook-circled--v1.png',
+      users: [1, 2, 3, 4, 5],
+    },
+    {
+      red: 'twiter',
+      url: 'https://img.icons8.com/color/150/000000/twitter-circled--v1.png',
+      users: [1, 2, 3, 4, 5],
+    },
+    {
+      red: 'instagram',
+      url: 'https://img.icons8.com/color/150/000000/instagram-new--v1.png',
+      users: [1, 2, 3, 4, 5],
+    },
+    {
+      red: 'youtube',
+      url: 'https://img.icons8.com/fluency/145/000000/youtube-play.png',
+      users: [1, 2, 3, 4, 5],
+    },
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
